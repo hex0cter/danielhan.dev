58fa9e0993b86481104de82fdae006b30a919712
Daniel Han <hex0cter@gmail.com>
2021-02-27 18:20:33 +0100

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ class Home extends React.Component {
           <p>Automation. Tooling. DevOps. This is me.</p>
           <p>I am a software engineer based in Stockholm. I like creating software that solves difficult problems, making tools that simplify other people's daily work, and building solutions that enable the rapid software delivery pipeline.</p>
           <p>I work with containers based cloud services on a daily basis. And I am comfortable programming with C++, Java, Javascript, Python, Ruby, Bash, etc. </p>
-          <p>Currently I am actively working with Ruby, Javascript, Docker, Kubernetes, Amazon Web Services (AWS), Jenkins, React, Selenium, TestCafe, Cucumber, etc.</p>
+          <p>Currently I am actively working with Python, Javascript, Docker, Kubernetes, Amazon Web Services (AWS), Jenkins, React, Selenium, TestCafe, Cucumber, etc.</p>
         </div>
         <div className='footer'>All rights reserved by Daniel Han.</div>
       </div>
