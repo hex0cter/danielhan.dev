9bf8bd1a1c964ec2eadb4fcacd8571a462077006
Daniel Han <daniel.han@scrive.com>
2021-04-27 11:46:56 +0200

    Add link in sidebar

--- src/components/SideBarList/SideBarList.jsx
+++ src/components/SideBarList/SideBarList.jsx
@@ -5,7 +5,8 @@ function SideBarList () {
   return (
     <div>
       <SideBarItem url='/home' icon='home'>Home</SideBarItem>
-      <SideBarItem url='https://notes.danielhan.dev/' icon='edit'>Quick note</SideBarItem>
+      <SideBarItem url='https://carnet.danielhan.dev/' icon='edit'>Tech Notes</SideBarItem>
+      <SideBarItem url='https://notes.danielhan.dev/' icon='edit'>notepad</SideBarItem>
       <SideBarItem url='https://github.com/hex0cter/' icon='github'>GitHub</SideBarItem>
       <SideBarItem url='https://www.linkedin.com/in/xdanielhan/' icon='linkedin'>LinkedIn</SideBarItem>
     </div>
