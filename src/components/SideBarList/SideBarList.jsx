6b9fc35546e924c5bb49c12503757f70200c6a00
Daniel Han <daniel.han@scrive.com>
2021-05-16 21:23:08 +0200

    Add web2md.danielhan.dev

--- src/components/SideBarList/SideBarList.jsx
+++ src/components/SideBarList/SideBarList.jsx
@@ -5,8 +5,9 @@ function SideBarList () {
   return (
     <div>
       <SideBarItem url='/home' icon='home'>Home</SideBarItem>
-      <SideBarItem url='https://carnet.danielhan.dev/' icon='edit'>Tech Notes</SideBarItem>
-      <SideBarItem url='https://notes.danielhan.dev/' icon='edit'>notepad</SideBarItem>
+      <SideBarItem url='https://web2md.danielhan.dev/' icon='file-markdown'>Web to markdown</SideBarItem>
+      <SideBarItem url='https://carnet.danielhan.dev/' icon='clock-circle'>Tech notes</SideBarItem>
+      <SideBarItem url='https://notes.danielhan.dev/' icon='edit'>Notepad</SideBarItem>
       <SideBarItem url='https://github.com/hex0cter/' icon='github'>GitHub</SideBarItem>
       <SideBarItem url='https://www.linkedin.com/in/xdanielhan/' icon='linkedin'>LinkedIn</SideBarItem>
     </div>
